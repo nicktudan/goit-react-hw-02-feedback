c74f7d86390172123e499f3ad15d01378ae1745e
nicktudan <107770913+nicktudan@users.noreply.github.com>
2023-02-27 00:00:07 +0100

    Change styled Layot

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 export const Layout = styled.div`
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontSize: 40,
-    color: '#010101',
+    /* height: 100vh; */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 24px;
 `;
